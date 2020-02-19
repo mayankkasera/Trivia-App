f36c048ea0f3223eb6faeaca1a5640e099312773
mayankkasera <mayankkasera98@gmail.com>
2020-02-19 23:27:55 +0530

    init

---
+++ app/src/main/java/com/mayankkasera/triviaapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mayankkasera.triviaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
